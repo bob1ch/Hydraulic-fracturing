1c0a33edb7a42006018a5143e842f8c12f1f83b9
SergeiShevyrev <71576182+SergeiShevyrev@users.noreply.github.com>
2020-12-18 18:37:40 +1000

    Add files via upload
    add dummy engine class and main.py - starter class

---
+++ py/engine.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Engine():
+    def __init__(self):
+        print('the Engine has been initialized');
+        
+    def compute(self):
+        print('the computations have been activated...');
+        """
+        TODO:
+            Разместить здесь код с вычислениями, пусть параметры будут статичны первое время.
+            Затем мы найдем способ вводить данные в Engine
+        """
